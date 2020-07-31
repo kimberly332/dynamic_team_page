29a62d031a5f7c26bdbb865713337e614c2720ea
kimberly332 <kimberly.wang1010@gmail.com>
2020-07-31 01:40:19 -0400

    switch images bug fixed

--- js/main.js
+++ js/main.js
@@ -8,15 +8,33 @@
       name = document.querySelector(".name"),
       selfIntro = document.querySelector(".selfIntroduction"),
       contents = document.querySelectorAll(".contentContainer"),
-      images = document.querySelector(".popup-photo");
+      img = document.querySelector(".photo");
+
+  let buttonNum = 0;
 
   const personalInfo = [
   ["KIMBERLY", `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac quam sit amet elit lacinia pellentesque at sit amet quam. Cras id tincidunt ante, nec viverra eros. Proin nec nisl blandit, luctus leo eget, convallis dui. In ac lorem ac nulla sollicitudin consectetur vitae ut elit. Duis nisi mi, vestibulum quis luctus sit amet, consectetur rutrum lorem. Nullam molestie et ex sit amet dapibus. Fusce ultrices orci quis finibus vestibulum. Mauris augue nunc, consequat id nisl vitae, varius pretium purus.`],
   ["JENNY", `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac quam sit amet elit lacinia pellentesque at sit amet quam. Cras id tincidunt ante, nec viverra eros. Proin nec nisl blandit, luctus leo eget, convallis dui. In ac lorem ac nulla sollicitudin consectetur vitae ut elit. Duis nisi mi, vestibulum quis luctus sit amet, consectetur rutrum lorem. Nullam molestie et ex sit amet dapibus. Fusce ultrices orci quis finibus vestibulum. Mauris augue nunc, consequat id nisl vitae, varius pretium purus.`]  
   ]
 
-  function showLightbox() {
+  function changeImage() {
+    console.log(buttonNum);
+    if (buttonNum == 1) {
+      img.src =`images/member1.jpg`;
+    }
+    else {
+      img.src =`images/member2.jpg`;
+    }
+  }
+
+  function showLightbox(event) {
     popUp.classList.add('showPopup');
+    console.log(this.id);
+    if (this.id == "a"){
+      buttonNum = 1;
+    } else {
+      buttonNum = 2;
+    }
   }
 
   function hideLightbox() {
@@ -26,15 +44,16 @@
   function showContent() {
     name.textContent = `${personalInfo[this.dataset.offset][0]}`;
     selfIntro.textContent = `${personalInfo[this.dataset.offset][1]}`;
+    changeImage();
   }
 
-  function changeImage() {
-    
-  }
+  buttons1.forEach(button1 => {
+    button1.addEventListener("click", showLightbox)
 
-
-  buttons1.forEach(button1 => button1.addEventListener("click", showLightbox));
-  buttons2.forEach(button2 => button2.addEventListener("click", showLightbox));
+  });
+  buttons2.forEach(button2 => {
+    button2.addEventListener("click", showLightbox)
+  });
   contents.forEach(content => content.addEventListener("click", showContent));
   lbClose.addEventListener("click", hideLightbox);
 })();
