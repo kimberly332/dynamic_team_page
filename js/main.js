6837afe9c0707b93d1fe01f2411b465550ef396b
kimberly332 <kimberly.wang1010@gmail.com>
2020-08-05 22:35:59 -0400

    renamed image

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
   
   let popUp = document.querySelector(".popup"),
       lbClose = popUp.querySelector("span"),
-      buttons1 = document.querySelectorAll(".button1"),
-      buttons2 = document.querySelectorAll(".button2"),
+      button1 = document.querySelector(".button1"),
+      button2 = document.querySelector(".button2"),
       name = document.querySelector(".name"),
       selfIntro = document.querySelector(".selfIntroduction"),
       contents = document.querySelectorAll(".contentContainer"),
@@ -13,12 +13,11 @@
   let buttonNum = 0;
 
   const personalInfo = [
-  ["KIMBERLY", `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac quam sit amet elit lacinia pellentesque at sit amet quam. Cras id tincidunt ante, nec viverra eros. Proin nec nisl blandit, luctus leo eget, convallis dui. In ac lorem ac nulla sollicitudin consectetur vitae ut elit. Duis nisi mi, vestibulum quis luctus sit amet, consectetur rutrum lorem. Nullam molestie et ex sit amet dapibus. Fusce ultrices orci quis finibus vestibulum. Mauris augue nunc, consequat id nisl vitae, varius pretium purus.`],
-  ["JENNY", `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac quam sit amet elit lacinia pellentesque at sit amet quam. Cras id tincidunt ante, nec viverra eros. Proin nec nisl blandit, luctus leo eget, convallis dui. In ac lorem ac nulla sollicitudin consectetur vitae ut elit. Duis nisi mi, vestibulum quis luctus sit amet, consectetur rutrum lorem. Nullam molestie et ex sit amet dapibus. Fusce ultrices orci quis finibus vestibulum. Mauris augue nunc, consequat id nisl vitae, varius pretium purus.`]  
+  ["KIMBERLY", `Lorem1 ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac quam sit amet elit lacinia pellentesque at sit amet quam. Cras id tincidunt ante, nec viverra eros. Proin nec nisl blandit, luctus leo eget, convallis dui. In ac lorem ac nulla sollicitudin consectetur vitae ut elit. Duis nisi mi, vestibulum quis luctus sit amet, consectetur rutrum lorem. Nullam molestie et ex sit amet dapibus. Fusce ultrices orci quis finibus vestibulum. Mauris augue nunc, consequat id nisl vitae, varius pretium purus.`],
+  ["JENNY", `Lorem2 ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac quam sit amet elit lacinia pellentesque at sit amet quam. Cras id tincidunt ante, nec viverra eros. Proin nec nisl blandit, luctus leo eget, convallis dui. In ac lorem ac nulla sollicitudin consectetur vitae ut elit. Duis nisi mi, vestibulum quis luctus sit amet, consectetur rutrum lorem. Nullam molestie et ex sit amet dapibus. Fusce ultrices orci quis finibus vestibulum. Mauris augue nunc, consequat id nisl vitae, varius pretium purus.`]  
   ]
 
   function changeImage() {
-    console.log(buttonNum);
     if (buttonNum == 1) {
       img.src =`images/show1.jpg`;
     }
@@ -29,7 +28,6 @@
 
   function showLightbox(event) {
     popUp.classList.add('showPopup');
-    console.log(this.id);
     if (this.id == "a"){
       buttonNum = 1;
     } else {
@@ -47,13 +45,8 @@
     changeImage();
   }
 
-  buttons1.forEach(button1 => {
-    button1.addEventListener("click", showLightbox)
-
-  });
-  buttons2.forEach(button2 => {
-    button2.addEventListener("click", showLightbox)
-  });
+  button1.addEventListener("click", showLightbox);
+  button2.addEventListener("click", showLightbox);
   contents.forEach(content => content.addEventListener("click", showContent));
   lbClose.addEventListener("click", hideLightbox);
 })();
